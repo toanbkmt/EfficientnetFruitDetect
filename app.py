8b28fcdce83120538bb05a2103baa25fe2dcb914
Toàn Vinh <vinhtoan.tran@aestechgroup.com>
2020-07-09 11:13:52 +0700

    add main class run app

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, request, render_template
+from base64 import b64encode
+app = Flask(__name__)
+
+from commons import get_tensor
+from inference import get_fruit_name
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        return render_template('home.html', value='index')
+    if request.method == 'POST':
+        # check have file selected ?
+        file = request.files['file']      
+        if request.files['file'].filename == '':
+            print('file not uploaded')
+            return render_template('home.html', value='index')        
+        image = file.read()
+        #show image in view result
+        image_base_64_result = b64encode(image).decode("utf-8")
+        top_probs, top_labels, top_fruits = get_fruit_name(image_bytes=image)
+        #prediction, probs = get_fruit_name(image_bytes=image)
+        get_fruit_name(image_bytes=image)
+        tensor = get_tensor(image_bytes=image)
+        print(get_tensor(image_bytes=image))      
+        return render_template('prediction.html', fruits=top_fruits, name=top_labels, probabilities=top_probs, imagebase64=image_base_64_result)
+
+if __name__ == '__main__':
+	app.run(debug=True)
